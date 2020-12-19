24d2e447aefc9eb19302c2027fdc30d8eea7af3f
Ben Elferink <ben.elferink@icloud.com>
2020-12-19 20:55:59 +0200

    Cleaned some lines from nodemailer

--- backend/controllers/register.js
+++ backend/controllers/register.js
@@ -25,20 +25,17 @@ export const register = async (req, res) => {
   }
 };
 
-// async..await is not allowed in global scope, must use a wrapper
+// basically I copied this function from nodemailer.com and pasted the code here,
+// I must mention that I do understand the code, and edited the parts needed.
 async function sendThankYouMail(form) {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
+  // create reusable transporter object (using the default SMTP transport)
   let transporter = nodemailer.createTransport({
     host: process.env.SMTP_SERVER,
     port: process.env.SMTP_PORT,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: process.env.SMTP_USER, // generated ethereal user
-      pass: process.env.SMTP_PASS, // generated ethereal password
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
   });
 
@@ -59,8 +56,4 @@ async function sendThankYouMail(form) {
 
   console.log('✅ Message sent: %s', info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
